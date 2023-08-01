025e0a901dde7308afb06fe3f34f665a215186a2
BudHamud <nigitart@gmail.com>
2023-07-31 23:19:04 -0300

    cambios para netlify

--- src/components/CardProds.jsx
+++ src/components/CardProds.jsx
@@ -24,7 +24,7 @@ const CardProduct = styled.article`
     text-orientation: upright;
     position: absolute;
     pointer-events: none;
-    color: #3C4E90;
+    color: #3c4e90;
     font-weight: bolder;
     background-color: white;
     border: solid black 1px;
@@ -51,38 +51,31 @@ const CardProduct = styled.article`
     border: rgb(0, 0, 0) solid 1px;
     margin-top: 0.7rem;
     border-radius: 1rem;
-    text-align: start;
     display: flex;
-    flex-flow: column;
-    align-items: center;
-    justify-content: space-around;
-
-    h3 {
-      margin-top: 1rem;
-      margin-right: 6rem;
-      align-self: flex-center;
-    }
-
-    a {
-      text-decoration: none;
-      cursor: pointer;
-      margin-right: 0.5rem;
-      margin-bottom: 1rem;
-      font-size: smaller;
-      font-weight: bold;
-      color: white;
-      background-color: #75b46a;
-      align-self: flex-end !important;
-      border-radius: 0.8rem;
-      padding: 0.5rem 1.5rem 0.5rem 1.5rem;
-      margin-left: 0.8rem;
-      border: rgb(0, 0, 0) solid 1px;
-      box-shadow: rgba(0, 0, 0, 1) 2px 3px 0px 1px;
-
-      :active {
+    flex-direction: column;
+    justify-content: space-between;
+    .container-footer {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 20px;
+      a {
+        text-decoration: none;
+        cursor: pointer;
+        font-size: smaller;
+        font-weight: bold;
         color: white;
-        transform: scale(0.9);
-        box-shadow: rgba(0, 0, 0, 1) 1px 1px 0px 1px;
+        background-color: #75b46a;
+        border-radius: 0.8rem;
+        padding: 0.5rem 1.5rem 0.5rem 1.5rem;
+        border: rgb(0, 0, 0) solid 1px;
+        box-shadow: rgba(0, 0, 0, 1) 2px 3px 0px 1px;
+
+        :active {
+          color: white;
+          transform: scale(0.9);
+          box-shadow: rgba(0, 0, 0, 1) 1px 1px 0px 1px;
+        }
       }
     }
   }
@@ -92,15 +85,17 @@ export default function CardProds({ item }) {
   return (
     <CardProduct>
       <div className="container-img">
-        <img src={ item.img } alt="imgProd" />
+        <img src={item.img} alt="imgProd" />
       </div>
 
-      <h5>{ item.brand }</h5>
+      <h5>{item.brand}</h5>
 
       <div className="container-info">
-        <h3>{ item.name }</h3>
-        <p>{ item.price }</p>
-        <Link to={`/products/${item._id}`}>Comprar</Link>
+        <h3>{item.name}</h3>
+        <div className="container-footer">
+          <p>{item.price}</p>
+          <Link to={`/products/${item._id}`}>Comprar</Link>
+        </div>
       </div>
     </CardProduct>
   );
